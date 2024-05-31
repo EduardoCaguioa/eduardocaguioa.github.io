85b7313fa34189e3ac9a91f11f4b34122862c608
EduardoCaguioa <monderineduard@gmail.com>
2024-05-31 20:05:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const btnLike1 = document.getElementById("btnlike1")
 const count1 = document.getElementById("count1")
 const btnDislike1 = document.getElementById("btndislike1")
 const dcount1 = document.getElementById("dcount1")
-
 const btnLike2 = document.getElementById("btnlike2")
 const count2 = document.getElementById("count2")
 const btnDislike2 = document.getElementById("btndislike2")
@@ -16,7 +15,7 @@ btnLike1.addEventListener("click",countLike1)
 
 function countDislike1() {
   let dislikeCounts = parseInt(dcount1.value) + 1
-  count1.textContent = dislikeCounts.toString()
+  dcount1.textContent = dislikeCounts.toString()
 }
 btnDislike1.addEventListener("click",countDislike1)
 
@@ -28,7 +27,7 @@ btnLike2.addEventListener("click",countLike2)
 
 function countDislike2() {
   let dislikeCounts = parseInt(dcount2.value) + 1
-  count2.textContent = dislikeCounts.toString()
+  dcount2.textContent = dislikeCounts.toString()
 }
 btnDislike2.addEventListener("click",countDislike2)
 
