6a41f4de4921f3f5a7144e6507d45d0b46a7c1a8
EduardoCaguioa <monderineduard@gmail.com>
2024-05-31 19:30:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const btnLike1 = document.getElementById("btnlike1")
 const btnDislike1 = document.getElementById("btndislike1")
 const count1 = document.getElementById("count1")
+const dcount1 = document.getElementById("dcount1")
 const btnLike2 = document.getElementById("btnlike2")
 const btnDislike2 = document.getElementById("btndislike2")
 const count2 = document.getElementById("count2")
+const dcount2 = document.getElementById("dcount2")
 
 function countLike1() {
   let likeCounts = parseInt(count1.value) + 1
